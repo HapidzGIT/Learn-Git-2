9edb1928fdb884ec3afc7679f9fd442fe7a6720c
HapidzGIT <hpsgaming212@gmail.com>
2023-12-26 10:49:21 +0700

    Menambahkan button

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Hello ALL</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quam odit ex voluptates aut magnam perspiciatis alias adipisci, soluta quod optio laborum dolorum excepturi illo mollitia consectetur, aspernatur deleniti ullam?</p>
+    <button>Rarwrr</button>
 </body>
 </html>
